0ae5bd6d33dd73f51196698f7920ad51569610a6
Nelson W Foltz <nelsonwfoltz@Nelsons-iMac.fios-router.home>
2020-01-03 14:41:32 -0500

    first commit

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+// user: user
+// password: meanStack1234
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+mongoose
+  .connect(
+    'mongodb+srv://user:' +
+      process.env.MONGO_ATLAS_PW +
+      '@udemy-mean-anwnk.mongodb.net/node-angular?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('connected to database');
+  })
+  .catch(() => {
+    console.log('connection failed');
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/images', express.static(path.join('images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.use('/api/posts', postRoutes);
+app.use('/api/user', userRoutes);
+
+module.exports = app;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:
+    'http://nodeangular-env-2.tu9pfzcngv.us-east-2.elasticbeanstalk.com/api'
 };
